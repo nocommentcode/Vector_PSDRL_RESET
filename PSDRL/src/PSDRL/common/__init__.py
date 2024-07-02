2d8c765695de2e7bff567abcd7a8348834f4eea8
Malo Hamon <hamon.malo@gmail.com>
2024-07-02 12:33:34 +1000

    updatesss

---
+++ PSDRL/src/PSDRL/common/__init__.py
@@ -0,0 +1,7 @@
+from gymnasium.envs.registration import register
+
+register(
+    id="MiniGrid-Empty-5x5-v1",
+    entry_point="minigrid.envs:EmptyEnv",
+    kwargs={"size": 5, "max_steps": 1000},
+)
