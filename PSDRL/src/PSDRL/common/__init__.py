2bd94a17ef2f0902f08988f53c417a07473766d7
Malo Hamon <hamon.malo@gmail.com>
2024-07-02 20:13:59 +1000

    8x8

--- PSDRL/src/PSDRL/common/__init__.py
+++ PSDRL/src/PSDRL/common/__init__.py
@@ -5,3 +5,10 @@ register(
     entry_point="minigrid.envs:EmptyEnv",
     kwargs={"size": 5, "max_steps": 300},
 )
+
+
+register(
+    id="MiniGrid-Empty-8x8-v1",
+    entry_point="minigrid.envs:EmptyEnv",
+    kwargs={"size": 8, "max_steps": 700},
+)
